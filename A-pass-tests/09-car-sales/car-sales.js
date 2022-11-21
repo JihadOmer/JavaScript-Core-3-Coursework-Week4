f501963b431d5be491c8b7d525ad92b880956926
Jihad Omer <jhjahony@gmail.com>
2022-11-21 10:44:33 +0000

    get total done

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,9 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let total = 0;
+    for (let i = 0; i < carsSold.length; i++) {
+        total += carsSold[i];
+    }
+    return total;
+}
 
 module.exports = sales;
