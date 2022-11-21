b4d06f79202184ceb90047970bc44aa379e2f6c5
Jihad Omer <jhjahony@gmail.com>
2022-11-21 20:38:42 +0000

    Largest number done

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,18 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  let words = ["Irina", "Etza", "Daniel"];
+
   // Act
+  let result = removeVowelsFromWords(words);
+
+
   // Assert
+
+  expect(result).toEqual(["__i_a", "_t_z_", "_a_ie_"]);
+  
+  // expect(result).toEqual(["rn", "tz", "nl"]);
+
 });
 
 // example
--- B-write-tests/06-largest-number/largest-number.test.js
+++ B-write-tests/06-largest-number/largest-number.test.js
@@ -2,12 +2,17 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  let numbers = [3, 21, 88, 4, 36];
   // Act
+  let result = getLargestNumber(numbers);
   // Assert
+
+  expect(result).toEqual(88)
 });
 
 // example
 // input: [3, 21, 88, 4, 36];
 // expected: 88;
+
 
 // also test that the original array hasn't changed
